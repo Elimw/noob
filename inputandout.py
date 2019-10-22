5205bed07973aa277d8c614d0f3537ec88c05736
Elimw <1329286049@qq.com>
2019-10-22 17:00:21 +0800

    输入与输出的python文件

---
+++ inputandout.py
@@ -0,0 +1,18 @@
+#输出： 打印到屏幕上一些信息
+#可以接受多个字符串，用逗号分隔
+#在写python程序时切忌不要使用中文字符。
+print("Elimw is a handsome man !","Elimw will never die,just fly",18)
+
+#输入 ： 从外部获取变量的值
+#input:阻塞，等待输入
+name = input()
+print("-----------------------------")
+print(name)
+
+
+
+
+
+
+
+
